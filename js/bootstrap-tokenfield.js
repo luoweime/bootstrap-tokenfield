83b9ed39f8b054675b8a7dcc7f6e85d89a1527ae
weiluo <weiluo@thoughtworks.com>
2014-04-02 21:52:49 +0800

    modify width setting

--- js/bootstrap-tokenfield.js
+++ js/bootstrap-tokenfield.js
@@ -898,7 +898,12 @@
         if (this.textDirection === 'rtl') {
           return this.$input.width( this.$input.offset().left + this.$input.outerWidth() - this.$wrapper.offset().left - parseInt(this.$wrapper.css('padding-left'), 10) - inputPadding - 1 )
         }
-        this.$input.width( this.$wrapper.offset().left + this.$wrapper.width() + parseInt(this.$wrapper.css('padding-left'), 10) - this.$input.offset().left - inputPadding )
+        var preWidth = this.$wrapper.offset().left + this.$wrapper.width() + parseInt(this.$wrapper.css('padding-left'), 10) - this.$input.offset().left - inputPadding;
+        if (this.$wrapper.find('.before-placeholder') && (this.$wrapper.width() - preWidth <= 16)) {
+          this.$input.width(preWidth - 16);
+        } else {
+          this.$input.width(preWidth);
+        }
       }
     }
 
